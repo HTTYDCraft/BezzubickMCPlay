b6f87631f3b702779b9da574fe0fab9b242d639f
BezzubickMCPlay <BezzubickMCPlay@users.noreply.github.com>
2026-09-12 13:33:21 +0000

    deploy: 4ed55ab791576a7233793151fe94ec7211baf6c6

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    BASE derived from scope instead of hardcoded. */
 'use strict';
 
-var CACHE = 'bezzubick-v2';
+var CACHE = 'bezzubick-20260912133320';
 var BASE = new URL(registration.scope).pathname.replace(/\/$/, '');
 
 var APP_SHELL = [
